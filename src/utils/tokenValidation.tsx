e501fbec54d716414435c501c1e0509ce0cffdcf
jrtate <47283674+jrtate@users.noreply.github.com>
2023-11-16 12:40:13 -0500

    Remove console logs

--- src/utils/tokenValidation.tsx
+++ src/utils/tokenValidation.tsx
@@ -1,11 +1,6 @@
 export const isTokenValid = () => {
   const token = sessionStorage.getItem("token");
   const expiration = sessionStorage.getItem("expiration");
-  console.log("token", !!token);
-  console.log(
-    "new Date() < new Date(expiration)",
-    new Date() > new Date(expiration),
-  );
 
   return new Date() > new Date(expiration) && !!token;
 };
