c9723fb126a0b6abdfdded9a16c11d643fceb0fc
jrtate <47283674+jrtate@users.noreply.github.com>
2023-11-30 19:17:26 -0500

    Enter to login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -14,44 +14,51 @@ const Login = () => {
   const login = useLoginMutation(navigate, handleSetShowToast);
 
   return (
-    <LoginContainer padding={3}>
-      <Typography sx={{ mb: 2 }} variant="h6">
-        Login
-      </Typography>
-
-      <TextField
-        label="Email"
-        variant="standard"
-        value={email}
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        fullWidth
-      />
-      <TextField
-        sx={{ mt: 1 }}
-        label="Password"
-        variant="standard"
-        value={password}
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        fullWidth
-      />
-
-      <Box sx={{ display: "flex", justifyContent: "flex-end" }} mt={3}>
-        <Link to="/signup">
-          <Button onClick={() => {}}>New User? Sign up here!</Button>
-        </Link>
-        <LoadingButton
-          loading={false}
-          onClick={() => login.mutate({ email, password })}
-          disabled={!email || !password}
-        >
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        login.mutate({ email, password });
+      }}
+    >
+      <LoginContainer padding={3}>
+        <Typography sx={{ mb: 2 }} variant="h6">
           Login
-        </LoadingButton>
-      </Box>
-    </LoginContainer>
+        </Typography>
+
+        <TextField
+          label="Email"
+          variant="standard"
+          value={email}
+          // type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          fullWidth
+        />
+        <TextField
+          sx={{ mt: 1 }}
+          label="Password"
+          variant="standard"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          fullWidth
+        />
+
+        <Box sx={{ display: "flex", justifyContent: "space-between" }} mt={3}>
+          <Link to="/signup">
+            <Button onClick={() => {}}>New User? Sign up here!</Button>
+          </Link>
+          <LoadingButton
+            type="submit"
+            loading={login.isPending}
+            disabled={!email || !password}
+          >
+            Login
+          </LoadingButton>
+        </Box>
+      </LoginContainer>
+    </form>
   );
 };
 
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -13,43 +13,47 @@ const SignUp = () => {
   const [password, setPassword] = useState<string>("");
 
   return (
-    <SignUpContainer padding={3}>
-      <Typography sx={{ mb: 2 }} variant="h6">
-        Sign Up
-      </Typography>
-
-      <TextField
-        label="Email"
-        variant="standard"
-        value={email}
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        fullWidth
-      />
-      <TextField
-        sx={{ mt: 1 }}
-        label="Password"
-        variant="standard"
-        value={password}
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        fullWidth
-      />
-
-      <Box sx={{ display: "flex", justifyContent: "flex-end" }} mt={3}>
-        <Link to="/login">
-          <Button onClick={() => {}}>Back</Button>
-        </Link>
-        <Button
-          onClick={() => signUp.mutate({ email, password })}
-          disabled={!email || !password}
-        >
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        signUp.mutate({ email, password });
+      }}
+    >
+      <SignUpContainer padding={3}>
+        <Typography sx={{ mb: 2 }} variant="h6">
           Sign Up
-        </Button>
-      </Box>
-    </SignUpContainer>
+        </Typography>
+
+        <TextField
+          label="Email"
+          variant="standard"
+          value={email}
+          // type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          fullWidth
+        />
+        <TextField
+          sx={{ mt: 1 }}
+          label="Password"
+          variant="standard"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          fullWidth
+        />
+
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }} mt={3}>
+          <Link to="/login">
+            <Button onClick={() => {}}>Back</Button>
+          </Link>
+          <Button type="submit" disabled={!email || !password}>
+            Sign Up
+          </Button>
+        </Box>
+      </SignUpContainer>
+    </form>
   );
 };
 
